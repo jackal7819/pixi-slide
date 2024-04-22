4a7c6a54c0cf67b33b4ef7be9ade02bed7492fca
jackal7819 <jackal3166@gmail.com>
2024-04-22 19:17:36 +0300

    Add fetchImages

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
+	import { useQuery } from '@tanstack/vue-query';
+	import { ref } from 'vue';
+	import { getImages } from './services/fetchImages';
+	import Title from './components/Title.vue';
 	import ImageCarousel from './components/ImageCarousel.vue';
 	import SelectedImageListUrl from './components/SelectedImageListUrl.vue';
+
+	const page = ref(1);
+	const { isPending, isError, data, error } = useQuery({
+		queryKey: ['images'],
+		queryFn: () => getImages(page),
+	});
+	console.log(data);
 </script>
 
 <template>
-	<h1 class="text-3xl font-bold underline">Hello world!</h1>
-	<ImageCarousel />
-	<SelectedImageListUrl />
+	<div class="min-h-screen bg-gradient-to-r from-violet-200 to-pink-200">
+		<div
+			class="p-5 md:p-10 max-w-[1920px] mx-auto flex flex-col justify-between min-h-screen"
+		>
+			<Title />
+			<ImageCarousel />
+			<SelectedImageListUrl />
+		</div>
+	</div>
 </template>
---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex items-center justify-between">
+		<h1
+			class="text-5xl font-bold duration-300 cursor-default hover:text-amber-500"
+		>
+			PixiSlide
+		</h1>
+		<div className="flex">
+			<p
+				className="flex items-center justify-center p-1 text-xl font-semibold duration-300 bg-black rounded-md group hover:bg-amber-500 cursor-default"
+			>
+				<span
+					className="ml-1 mr-2 text-white duration-300 group-hover:text-white"
+				>
+					ViK
+				</span>
+				<span
+					className="flex items-center justify-center w-10 h-8 text-black duration-300 bg-white rounded group-hover:text-amber-500"
+				>
+					Tor
+				</span>
+			</p>
+		</div>
+	</div>
+</template>
